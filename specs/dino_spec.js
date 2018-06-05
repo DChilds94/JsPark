10f381299de59247e4290b5f3e5d8f821f43d0f4
Daniel Childs <dc.1994@outlook.com>
2018-06-05 18:00:31 +0100

    dino has a type

--- specs/dino_spec.js
+++ specs/dino_spec.js
@@ -1,3 +1,19 @@
 const assert = require("assert")
 const Dino = require("../dino.js")
 const Park = require("../park.js")
+
+describe('Dino', function(){
+
+let dino;
+
+  beforeEach(function(){
+    dino = new Dino()
+  });
+
+it('should be a Tyrannosaurus', function(){
+  const dino = new Dino();
+dino.type = "Tyrannosaurus"
+  assert.strictEqual(dino.type, "Tyrannosaurus")
+});
+
+});
