3c6a25bb268c86e412227f0a5b47905f0e4a9ecb
Daniel Childs <dc.1994@outlook.com>
2018-06-05 18:22:34 +0100

    refacktor dino_spec.js

--- specs/dino_spec.js
+++ specs/dino_spec.js
@@ -7,18 +7,18 @@ describe('Dino', function(){
 let dino;
 
   beforeEach(function(){
-    dino = new Dino()
+    dino1 = new Dino("Tyrannosaurus", 2)
   });
 
 it('should be a Tyrannosaurus', function(){
-  const dino = new Dino();
-dino.type = "Tyrannosaurus"
-  assert.strictEqual(dino.type, "Tyrannosaurus")
+  // const dino = new Dino();
+// dino.type = "Tyrannosaurus"
+  assert.strictEqual(dino1.type, "Tyrannosaurus")
 });
 
 it('should have 2 babies per year', function(){
-  dino.offspringPerYear = 2;
-  assert.strictEqual(dino.offspringPerYear, 2)
+  // dino.offspringPerYear = 2;
+  assert.strictEqual(dino1.offspringPerYear, 2)
 });
 
 });
